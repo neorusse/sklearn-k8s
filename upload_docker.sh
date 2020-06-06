0b2cb4b88ab44fda20988091e003853b6ecfec7b
neorusse <neorusse@gmail.com>
2020-06-06 21:38:17 +0100

    Push app image to registry

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="ecoden/sklearn-ml-api"
+
+# Step 2:
+# Authenticate & tag
+echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag $dockerpath:v1.0 $dockerpath:latest
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath:latest
